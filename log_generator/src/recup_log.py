e671d569b0a75fce44f00f8c9eae51b0f84fb7fe
Remi-Tortue <remiporee@orange.fr>
2022-10-11 10:51:55 +0200

    lumiere lvl sur affichage

--- log_generator/src/recup_log.py
+++ log_generator/src/recup_log.py
@@ -9,14 +9,16 @@ import math
 
 msg_compass = 0
 msg_alt = 0
-msg_battery = 0
 msg_joy_switch = 0
+lvl_lumiere = 0
 
 def listen():
     rospy.Subscriber("/mavros/global_position/compass_hdg", std_msgs.msg.Float64, callback_compass)  
     rospy.Subscriber("/mavros/global_position/rel_alt", std_msgs.msg.Float64, callback_alt)
-    rospy.Subscriber("/mavros/battery", sensor_msgs.msg.BatteryState, callback_battery)
     rospy.Subscriber("/num_joy_control", std_msgs.msg.Float64, callback_joy_switch)
+    rospy.Subscriber("/enregistrement_camera", std_msgs.msg.Float32MultiArray, callback_enregistrement) 
+    # /mavros/imu/data
+    # /joy sensor_msg/Joy
     
 def callback_compass(msg):
     global msg_compass
@@ -26,20 +28,20 @@ def callback_alt(msg):
     global msg_alt
     msg_alt = msg.data
 
-def callback_battery(msg):
-    global msg_battery
-    msg_battery = math.trunc(msg.voltage*100)/100
-
 def callback_joy_switch(msg):
     global msg_joy_switch
     msg_joy_switch = msg.data
+
+def callback_enregistrement(msg):
+    global lvl_lumiere 
+    lvl_lumiere = msg.data[1]
 
 def save_log_msg(log):
     log.write(
         "compass: "+str(msg_compass)+" \n"+
         "altitude: "+str(msg_alt)+" \n"+
-        "battery: "+str(msg_battery)+" \n"+
         "joy_control: "+str(msg_joy_switch)+" \n"+
+        "lumiere: "+str(lvl_lumiere)+" \n"+
         " \n"
     )
 
