2530fdde2470670bb615b09655070e3922a358f6
Dada462 <danut462@gmail.com>
2022-10-27 10:59:50 +0200

    USBL Async

--- guerledan_usbl/src/data_view_multiple_files.py
+++ guerledan_usbl/src/data_view_multiple_files.py
@@ -1,13 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-import os
-print(os.getcwd())
 
 
 values=['northing', 'easting', 'depth', 'azimith', 'elevation', 'range', 'Local depth']
 data_fusioned=[]
-for filename in glob.glob('/home/remi/catkin_ws/src/Niche-sauvage/guerledan_usbl/logs/October_*.dat'):
+for filename in glob.glob('logs/Mission_xy/October_*.dat'):
     f = open(filename, "r").read()
     print(filename)
     data=f.split('\n')
@@ -24,18 +22,17 @@ for filename in glob.glob('/home/remi/catkin_ws/src/Niche-sauvage/guerledan_usbl
 data_fusioned=np.array(data_fusioned)
 logs={}
 for i in range(len(values)):
-    # print(i)
     logs[values[i]]=data_fusioned[:,i]
 
 fig,ax=plt.subplots(figsize=(8,7))
 xlim=np.array([min(logs['northing']),max(logs['northing'])])
 ylim=np.array([min(logs['easting']),max(logs['easting'])])
-# guerledan_lake_sat_img = plt.imread("/home/remi/catkin_ws/src/Niche-sauvage/guerledan_usbl/logs/Map2.jpg")
+guerledan_lake_sat_img = plt.imread("logs/Map2.jpg")
 
 for i in range(len(logs['northing'])):
     if i%4==0:
         ax.clear()
-        # ax.imshow(guerledan_lake_sat_img, extent=[*(xlim*16/9), *ylim])
+        ax.imshow(guerledan_lake_sat_img, extent=[*(xlim*16/9), *ylim])
         ax.set_xlim(xlim)
         ax.set_ylim(ylim)
         ax.plot(logs['northing'][:i],logs['easting'][:i],color='#f11a1a')
