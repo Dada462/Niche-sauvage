610413172f4a37fbcaa9eb5c0ac85d46be267bba
hugo <hugo.reubrecht@ensta-bretagne.org>
2023-02-06 16:02:08 +0100

    algo control test

--- commande_rov_aruco/src/commande.py
+++ commande_rov_aruco/src/commande.py
@@ -37,7 +37,7 @@ def euler_from_quaternion(x, y, z, w):
      
         return roll_x, pitch_y, yaw_z # in radians
 
-def callback(data):
+def callback(data): ## callback qui recupere la pose du robot dans le repere du marqueur et renvoie la commande associée
     global id
     global x,y,z,roll_x, pitch_y, yaw_z
     global accel_x, accel_y, accel_z
@@ -45,26 +45,83 @@ def callback(data):
     y = data.pose.position.y
     z = data.pose.position.z
     roll_x, pitch_y, yaw_z = euler_from_quaternion(data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w)
-        
-    if id>=0 and id<=5 :
-        if z>0.7:
-            accel_z =0.3 ##avance
+    
+    """
+       disposition : 0 : interieur fond de la cage
+                     1 : interieur gauche
+                     2 : interieur droit
+                     3 : entrée gauche
+                     4 : entrée droite
+                     5 : entrée derriere gauche
+                     6 : entrée derriere droit
+                     7 : exterieur gauche
+                     8 : exterieur droit
+                     9 : non utilisé
+
+    """
+    ## Méthode bang bang
+
+    if id == 0 :   ## se placer devant
+        if z>0.3:
+            accel_z = 0.3 ##avance
             print("on avance")
-        if z<0.7:
-            accel_z = -0.3##recule 
+        if z<0.1:
+            accel_z = -0.3 ##recule 
             print("on recule")
-        if x > 0 :
+        if x > 0.3 :
+            accel_x = -0.3 ##droite
+            print("a droite")
+        if x < -0.3 :
+            accel_x = 0.3 ##gauche
+            print("a gauche")
+        if y > 0.3 :
+            accel_y = -0.3 ##bas
+            print("en bas")
+        if y < -0.3 :
+            accel_y = 0.3 ##haut
+            print("en haut")
+
+    elif id == 3 : ## se placer à droite
+        if z>0.8:
+            accel_z = 0.3 ##avance
+            print("on avance")
+        if z<0.1:
+            accel_z = -0.3 ##recule 
+            print("on recule")
+        if x > -0.5 :
             accel_x =-0.3 ##droite
             print("a droite")
-        if x < 0 :
-            accel_x = 0.3##gauche
-            print("a gauche")
-        if y > 0 :
-            accel_y = -0.3##bas
+        #if x < -0.3 :
+        #    accel_x = 0.3 ##gauche
+        #    print("a gauche")
+        if y > 0.3 :
+            accel_y = -0.3 ##bas
             print("en bas")
-        if y < 0 :
-            accel_y = 0.3##haut
+        if y < -0.3 :
+            accel_y = 0.3 ##haut
             print("en haut")
+
+    elif id == 4 : ## se placer à gauche
+        if z>0.8:
+            accel_z = 0.3 ##avance
+            print("on avance")
+        if z<0.1:
+            accel_z = -0.3 ##recule 
+            print("on recule")
+        #if x > -0.8 :
+        #    accel_x =-0.3 ##droite
+        #    print("a droite")
+        if x < 0.5 :
+            accel_x = 0.3 ##gauche
+            print("a gauche")
+        if y > 0.3 :
+            accel_y = -0.3 ##bas
+            print("en bas")
+        if y < -0.3 :
+            accel_y = 0.3 ##haut
+            print("en haut")
+
+
     else :
         accel_x = 0
         accel_y = 0
@@ -72,7 +129,7 @@ def callback(data):
         print("on stoppe")
  
 
-def callback2(data):
+def callback2(data): ## callback qui récupère l'id du marqueur
     global id
     id = data.data
 
